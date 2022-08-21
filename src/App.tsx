d87c4c01befd124a27e3283b6b0f35d310ed6878
Filip Pawłowicz <f.pawlowicz@medidok.de>
2022-08-21 22:17:38 +0200

    Initial commit'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Component, ReactNode } from 'react'
+import './App.css'
+
+export class App extends Component<any, any> {
+
+    render(): ReactNode {
+
+        return (
+            <div className="App">
+                <h1>CodeUp Filip Pawłowicz</h1>
+                <h1>NIP: 7123439240</h1>
+            </div>
+        )
+    }
+}
